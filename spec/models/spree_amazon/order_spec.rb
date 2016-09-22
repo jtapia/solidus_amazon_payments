9dc7476a6f3e4585d2615465078a9fb5090c7eb2
Allison C. Reilly <acreilly3@gmail.com>
2016-09-22 12:18:31 -0400

    Add close order reference on frontend
    - Add column closed_at to Spree::AmazonTransaction
    - Add close action to  Spree::AmazonTransaction
    - Add Spree::Payment::Processing#close! method
    - Refactored SpreeAmazon::Order#close_order_reference! to respond with ActiveMerchant::Billing::Response
    - Add amazon_payment to factories

--- spec/models/spree_amazon/order_spec.rb
+++ spec/models/spree_amazon/order_spec.rb
@@ -82,10 +82,9 @@ describe SpreeAmazon::Order do
             body: build_mws_close_order_reference_success_response,
           },
         )
-
         result = order.close_order_reference!
 
-        expect(result).to be_truthy
+        expect(result.message).to eq('Success')
       end
     end
 
@@ -98,10 +97,9 @@ describe SpreeAmazon::Order do
             body: build_mws_close_order_reference_failure_response,
           },
         )
+        result = order.close_order_reference!
 
-        expect {
-          order.close_order_reference!
-        }.to raise_error(SpreeAmazon::Order::CloseFailure)
+        expect(result.message).to eq( "404 InvalidOrderReferenceId: The OrderReferenceId ORDER_REFERENCE is invalid.")
       end
     end
   end
